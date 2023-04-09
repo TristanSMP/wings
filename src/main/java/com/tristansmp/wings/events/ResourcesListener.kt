b7d8f2fe19b6ff01dba07686375fa5d1bcfb4046
Tristan Camejo <contact@tristancamejo.com>
2023-04-09 22:37:21 +1000

    fix: get resource sha

--- src/main/java/com/tristansmp/wings/events/ResourcesListener.kt
+++ src/main/java/com/tristansmp/wings/events/ResourcesListener.kt
@@ -1,5 +1,9 @@
 package com.tristansmp.wings.events
 
+import com.tristansmp.wings.Wings
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.runBlocking
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -7,9 +11,18 @@ import org.bukkit.event.player.PlayerJoinEvent
 class ResourcesListener : Listener {
     @EventHandler()
     fun onPlayerJoin(event: PlayerJoinEvent) {
-        event.player.setResourcePack(
-            "https://re.tristansmp.com/resources.zip", null, true,
-        )
 
+        val scheduler = Wings.instance.server.scheduler
+
+        scheduler.runTaskAsynchronously(Wings.instance, Runnable {
+            runBlocking {
+                val res = Wings.instance.http.get("https://re.tristansmp.com/resources.zip.sha1")
+                val sha1 = res.body<String>()
+
+                event.player.setResourcePack(
+                    "https://re.tristansmp.com/resources.zip", sha1, true,
+                )
+            }
+        })
     }
 }
