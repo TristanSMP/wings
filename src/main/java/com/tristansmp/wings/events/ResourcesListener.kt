1436cef4abf691273ff2f75b907a7c434bf568b5
Tristan Camejo <contact@tristancamejo.com>
2023-04-10 09:21:11 +1000

    refactor: direct cdn, message

--- src/main/java/com/tristansmp/wings/events/ResourcesListener.kt
+++ src/main/java/com/tristansmp/wings/events/ResourcesListener.kt
@@ -4,6 +4,7 @@ import com.tristansmp.wings.Wings
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import kotlinx.coroutines.runBlocking
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -11,16 +12,27 @@ import org.bukkit.event.player.PlayerJoinEvent
 class ResourcesListener : Listener {
     @EventHandler()
     fun onPlayerJoin(event: PlayerJoinEvent) {
-
         val scheduler = Wings.instance.server.scheduler
 
         scheduler.runTaskAsynchronously(Wings.instance, Runnable {
             runBlocking {
-                val res = Wings.instance.http.get("https://re.tristansmp.com/resources.zip.sha1")
+                val res = Wings.instance.http.get("https://storage.googleapis.com/re.tristansmp.com/resources.zip.sha1")
                 val sha1 = res.body<String>()
 
                 event.player.setResourcePack(
-                    "https://re.tristansmp.com/resources.zip", sha1, true,
+                    "https://storage.googleapis.com/re.tristansmp.com/resources.zip", sha1, true,
+                    LegacyComponentSerializer.legacyAmpersand().deserialize(
+                        arrayOf(
+                            "&7&m------------------------------------",
+                            "&6Hey &d${event.player.name}&r&f!",
+                            "&6Our resource pack is required to play TSMP!",
+                            "",
+                            "&fWe promise to not change any vanilla textures, rather it",
+                            "&fpowers our custom items, blocks, and more!",
+                            "",
+                            "&7&m------------------------------------"
+                        ).joinToString("&r\n")
+                    )
                 )
             }
         })
