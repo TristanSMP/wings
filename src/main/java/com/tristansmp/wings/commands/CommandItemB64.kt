3e2292b5bdc671c6344fc035ea830d2891f2d251
Tristan Camejo <contact@tristancamejo.com>
2023-04-12 15:49:19 +1000

    feat: itemb64 command

---
+++ src/main/java/com/tristansmp/wings/commands/CommandItemB64.kt
@@ -0,0 +1,49 @@
+package com.tristansmp.wings.commands
+
+import com.tristansmp.wings.Wings
+import com.tristansmp.wings.lib.SerializeUtils
+import com.tristansmp.wings.lib.sendError
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.format.TextColor
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class CommandItemB64 : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+        if (sender !is Player) {
+            sender.sendMessage("You must be a player to use this command!")
+            return true
+        }
+
+        if (!Wings.instance.commandRatelimiter.canRunCommand(sender)) {
+            sender.sendError("You are sending commands too fast! Please wait a few seconds before trying again.")
+            return true
+        }
+
+        if (!sender.hasPermission("wings.itemb64")) {
+            sender.sendError("You don't have permission to use this command!")
+            return true
+        }
+
+        val heldItem = sender.inventory.itemInMainHand
+
+
+        if (heldItem == null) {
+            sender.sendError("You must be holding an item to use this command!")
+            return true
+        }
+
+        val b64Item = SerializeUtils.itemStackToBase64(heldItem)
+
+        sender.sendMessage(
+            Component.text("Click to copy to clipboard").color(TextColor.fromHexString("#00FF00")).clickEvent(
+                ClickEvent.copyToClipboard(b64Item)
+            )
+        )
+
+        return true
+    }
+}
