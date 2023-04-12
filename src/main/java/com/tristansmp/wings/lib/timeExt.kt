8f9e49ae0b301246e74a88b6957851a487496e2e
Tristan Camejo <contact@tristancamejo.com>
2023-04-12 15:56:07 +1000

    refactor: use DescParseTickFormat

--- src/main/java/com/tristansmp/wings/lib/timeExt.kt
+++ src/main/java/com/tristansmp/wings/lib/timeExt.kt
@@ -1,12 +1,9 @@
 package com.tristansmp.wings.lib
 
+import com.earth2me.essentials.utils.DescParseTickFormat
 import org.bukkit.World
 
 fun World.get12HourTime(): String {
-    val time = this.time
-    val hour = (time / 1000 + 6) % 24
-    val minute = (time % 1000) / 1000 * 60
-    val ampm = if (hour < 12) "am" else "pm"
-    val hour12 = if (hour == 0L) 12 else if (hour > 12) hour - 12 else hour
-    return "$hour12:${minute.toString().padStart(2, '0')}$ampm"
+    val fmt = DescParseTickFormat.format12(this.time)
+    return fmt.substring(0, fmt.length - 2) + fmt.substring(fmt.length - 2).lowercase()
 }
